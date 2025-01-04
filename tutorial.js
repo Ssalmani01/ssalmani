b84b23a432835bec6d97aeef56468efd9715ad1c
Ssalmani01 <lupanmykola0@gmail.com>
2025-01-04 21:58:21 +0200

    first commit

---
+++ tutorial.js
@@ -0,0 +1,42 @@
+console.log('js for chainikov'); // выводим текст в консоль
+let name = "Kolya"; // Задаём переменную
+let admin = "";
+admin = name; // одна переменная 
+console.log("Имя админа: " + admin);
+let n = 1488; // переменная числа
+console.log(n / 2);
+const BigInt = 123456789012345678901234567890n; // n = означение что это биг инт
+console.log(BigInt);
+
+let adminNameChecked = true; // Boolean - Бул, да или нет, true или false
+let admenAgeChecked = false;
+
+let biger = 4 > 1;
+console.log(biger);
+
+let age;
+age = null
+console.log(age); // null
+
+let age2;
+console.log(age2); // undefined
+
+typeof undefined;
+typeof "aboba";
+typeof false;
+typeof Math;
+typeof null;
+typeof alert;
+typeof 0;
+
+console.log (typeof null); // вывести тайпоф
+
+name2 = prompt("Введите ваше имя: ") // input window
+
+console.log('Ваше имя: ' + name2);
+
+let ask = confirm("Вы Коля?"); // ask window
+alert(ask);
+
+
+
